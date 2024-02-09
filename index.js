d94a5582afe8ee90e12d27eaf20f9f32813f31d7
faatimamir <faatima.mir@gmail>
2024-02-09 21:41:05 +0500

    all

--- index.js
+++
@@ -1,8 +0,0 @@
-unction openNavbar() { 
-	document.getElementById("sideNavigationBar") 
-		.style.width = "50%"; 
-} 
-function closeNavbar() { 
-	document.getElementById("sideNavigationBar") 
-		.style.width = "0%"; 
-}
